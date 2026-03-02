bc36d557604c1a3a8a997ff1dd80fe2ba9ae86a5
yashraj18219 <yashraj18219@gmail.com>
2026-03-03 02:01:14 +0530

    added path for vercel

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.MODE === "development"
+    ? "/api"
+    : import.meta.env.VITE_API_URL,
   headers: { 'Content-Type': 'application/json' },
 });
 
